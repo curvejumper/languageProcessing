b8b11ed593a17b0fc5f56897301d787d706024f7
curvejumper <ortiz.d.michael@gmail.com>
2016-04-10 22:17:29 -0700

    Initial commit

---
+++ src/languageprocessing/LineProcessor.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package languageprocessing;
+
+/*
+ * Copyright 2016: smanna@cpp.edu
+ * Please do not change any public method's header.
+ * Feel free to include your own methods/variables as required.
+ */
+import java.util.HashSet;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LineProcessor {
+    
+    
+    public LineProcessor() {
+        // Constructor
+        // TODO(student): Feel free to add your initialization code, if any.
+    }
+
+    private HashSet<String> findEmails(String line) {
+        // TODO(student): Write your regular expression based email
+        // extractor code here. Feel free to add as many as private method
+        // you need.
+        HashSet<String> email = new HashSet();
+        
+        String pattern = "()";
+
+        Pattern r = Pattern.compile(pattern);
+
+        Matcher m = r.matcher(line);
+
+        if (m.find()) {
+            email.add(m.group());
+        }
+        return new HashSet<String>();  // returning empty => no result found
+    }
+
+    private HashSet<String> findPhoneNumbers(final String line) {
+        // TODO(student): Write your regular expression based phone
+        // number extractor code here. Feel free to add as many as private method
+        // you need.
+        HashSet<String> phoneNumber = new HashSet();
+        
+        String pattern = "()";
+
+        Pattern r = Pattern.compile(pattern);
+
+        Matcher m = r.matcher(line);
+
+        if (m.find()) {
+            phoneNumber.add(m.group());
+        }
+        return new HashSet<String>();  // returning empty => no result found
+    }
+
+    public HashSet<String> processLine(String line) {
+        // You should not be modifying this method.
+        HashSet<String> email = findEmails(line);
+        HashSet<String> phone = findPhoneNumbers(line);
+        HashSet<String> email_n_phones = new HashSet<String>();
+        for (String e : email) {
+            email_n_phones.add("e\t" + e);
+        }
+        for (String p : phone) {
+            email_n_phones.add("p\t" + p);
+        }
+        return email_n_phones;
+    }
+}
